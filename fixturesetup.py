9c7df0a218b9f28925ee4ad2a22c97c02a2cdf90
Stavro Purdie <stavropurdie9@gmail.com>
2024-06-28 21:12:45 +1200

    More work done to fixture profile script
    It now ouputs in json ;)

--- fixturesetup.py
+++ fixturesetup.py
@@ -1,16 +1,20 @@
 ##Property of Stavro Purdie, 2024
-import configparser
+import json
+
+confirm = input("WARNING! | This program resets your entire fixture config file. To continue, please type 'YES' otherwise press ENTER for exit >> ")
+if confirm == '':
+    print('Configuration Utility Exiting, No settings have been changed')
+    quit()
 
 def fixturesetup():
-    profiles = {
-
-    }
+    profiles = {}
     print('FIXTURE PROFILE CONFIGURATION SETUP WIZARD')
     print("Advice for the mislead. THIS PROGRAM IS ONLY FOR USE WITH MOVING SPOTS (not just any moving head or old bro's chinese PAR64)")
+    print('WARNING!')
     fixturecount = input('Enter fixture count for DMX Universe 1 >> ')
     i = 1
     for fixture in fixturecount:
-        profilename = input(f'Enter name for fixture {i} of {fixturecount} >> ')
+        profilename = input(f"Enter name for fixture {i} of {fixturecount} with any whitespaces replaced with '-' >> ")
 
         print('Movement Parameter Config:\n')
         pan = input('Enter Pan Channel >> ')
@@ -21,14 +25,14 @@ def fixturesetup():
 
         print('Colour Parameter Config:\n')
         print(f"Does fixture {profilename} use a Color Wheel or have LED Colour Changing?")
-        colsel = input("Enter '1' for Colour Wheel or '2' for LED Colour Changing")
+        colsel = input("Enter '1' for Colour Wheel or '2' for LED Colour Changing >> ")
         if colsel == '1':
             print('Colour Wheel Selected:')
             colwhl = input('Enter Colour Wheel Channel >> ')
         if colsel == '2':
             print('LED Colour Changing Selected:')
             print(f'Does fixture {profilename} use RGB or CMY Colour Systems?')
-            method = input("Enter '1' for RGB, '2' for CMY")
+            method = input("Enter '1' for RGB, '2' for CMY >> ")
             if method == '1':
                 print('RGB Selected')
                 red = input('Enter Red Channel >> ')
@@ -40,7 +44,7 @@ def fixturesetup():
                 magenta = input('Enter Magenta Channel >> ')
                 yellow = input('Enter Yellow Channel >>')
         print('Does the fixture have CTO (Colour Temp) Control?')
-        ctosel = input("Enter '1' for NO, '2' for YES")
+        ctosel = input("Enter '1' for NO, '2' for YES >> ")
         if ctosel == '2':
             cto = input('Enter CTO Channel >> ')
         
@@ -72,19 +76,46 @@ def fixturesetup():
         print(f'Beam Parameters: \n Zoom: {zoom}\n Focus: {focus}\n Frost: {frost}\n Static Gobo: {staticgobo}\n Rotating Gobo (No rotate function): {rotgobo}\n')
         print(f'Dimmer Parameters: \n Dimmer: {dimmer} \n Fine Dimmer {dimmerfine}\n')
 
+        profiles[profilename] = {}
+        profiles[profilename]['movement'] = {}
         profiles[profilename]['movement']['pan'] = pan
-        profiles[profilename]['movement']['panfine'] = panfine
+        profiles[profilename]['movement']['pan_fine'] = panfine
         profiles[profilename]['movement']['tilt'] = tilt
-        profiles[profilename]['movement']['tiltfine'] = tiltfine
-        profiles[profilename]['movement']['ptspeed'] = ptspeed
+        profiles[profilename]['movement']['tilt_fine'] = tiltfine
+        profiles[profilename]['movement']['pan_tilt_speed'] = ptspeed
         if colsel == '1':
-            profiles[profilename]['colour']['conventional']['colwhl'] = colwhl
+            profiles[profilename]['colour'] = {}
+            profiles[profilename]['colour']['conventional'] = {}
+            profiles[profilename]['colour']['conventional']['colour_wheel'] = colwhl
         if colsel == '2':
             if method == '1':
+                profiles[profilename]['colour'] = {}
+                profiles[profilename]['colour']['led'] = {}
+                profiles[profilename]['colour']['led']['rgb'] = {}
                 profiles[profilename]['colour']['led']['rgb']['red'] = red
                 profiles[profilename]['colour']['led']['rgb']['green'] = green
                 profiles[profilename]['colour']['led']['rgb']['blue'] = blue
             if method == '2':
+                profiles[profilename]['colour'] = {}
+                profiles[profilename]['colour']['led'] = {}
+                profiles[profilename]['colour']['led']['cmy'] = {}
+                profiles[profilename]['colour']['led']['cmy']['cyan'] = cyan
+                profiles[profilename]['colour']['led']['cmy']['magenta'] = magenta
+                profiles[profilename]['colour']['led']['cmy']['yellow'] = yellow
+        if ctosel == '2':
+            profiles[profilename]['colour']['cto'] = cto
+        profiles[profilename]['beam'] = {}
+        profiles[profilename]['beam']['zoom'] = zoom
+        profiles[profilename]['beam']['focus'] = focus
+        profiles[profilename]['beam']['frost'] = frost
+        profiles[profilename]['beam']['static_gobo'] = staticgobo
+        profiles[profilename]['beam']['rotating_gobo'] = rotgobo
+        profiles[profilename]['dimmer'] = {}
+        profiles[profilename]['dimmer']['dimmer'] = dimmer
+        profiles[profilename]['dimmer']['dimmer_fine'] = dimmerfine
+    
+    with open('fixtureprofiles.txt', 'w') as convert_file: 
+        convert_file.write(json.dumps(profiles, indent=4))
 
-        
-
+if __name__ == "__main__":
+    fixturesetup()
