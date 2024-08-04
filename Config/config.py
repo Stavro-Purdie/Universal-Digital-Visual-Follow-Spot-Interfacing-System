2288c876747e2895cb866cfb741e8bfaaab2fd43
Stavro Purdie <stavropurdie9@gmail.com>
2024-08-04 21:49:33 +1200

    Nearly finished init section of GUI backend

--- Config/config.py
+++ Config/config.py
@@ -1,22 +1,36 @@
+## This program runs the config GUI
 import sys
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QFile, QIODevice
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+def runaddfixtureprofile():
+    pass
 
-    ui_file_name = "Config.ui"
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
-        sys.exit(-1)
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
-    window.show()
+## Init GUI files....
+app = QApplication(sys.argv)
+loader = QUiLoader()
 
-    sys.exit(app.exec())
+## Main config UI....
+mainconfiguifile = QFile("Config.ui")
+configui = loader.load(mainconfiguifile)
+mainconfiguifile.close()
+if not configui:
+    print(configui.errorString())
+    sys.exit(-1)
+
+## Add Fixture Profile UI....
+addfixtureprofilefile = QFile("addfixtureprofile.ui")
+addfixtureprofile = loader.load(addfixtureprofilefile)
+addfixtureprofilefile.close()
+if not addfixtureprofile:
+    print(addfixtureprofile.errorString())
+    sys.exit(-1)
+
+## Fire up the GUI and show it
+configui.show()
+
+## If add fixture button pressed, run addfixtureprofile()
+configui.addfixtureprofile.clicked.connect(runaddfixtureprofile)
+
+sys.exit(app.exec())
