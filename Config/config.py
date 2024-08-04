0cf885615f1e7711ce12defc3654af35d551308f
Stavro Purdie <stavropurdie9@gmail.com>
2024-08-04 22:57:11 +1200

    Did more work on the backend, logic is very very hard.

--- Config/config.py
+++ Config/config.py
@@ -1,11 +1,13 @@
 ## This program runs the config GUI
-import sys
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QFile, QIODevice
+import serial.tools.list_ports
+import sys
+
 
 def runaddfixtureprofile():
-    pass
+    addfixtureprofile.show()
 
 ## Init GUI files....
 app = QApplication(sys.argv)
@@ -27,8 +29,29 @@ if not addfixtureprofile:
     print(addfixtureprofile.errorString())
     sys.exit(-1)
 
+## Fixture Patch UI....
+fixturepatchfile = QFile("fixturepatch.ui")
+fixturepatch = loader.load(fixturepatchfile)
+fixturepatchfile.close()
+if not fixturepatchfile:
+    print(fixturepatchfile.errorString())
+    sys.exit(-1)
+
 ## Fire up the GUI and show it
 configui.show()
+
+## Adapter Selection Init (Get all adapters)
+
+serialports = {}
+for port in serial.tools.list_ports.comports():
+    serialports[port.name] = {}
+    serialports[port.name]['description'] = port.description
+    serialports[port.name]['manufacturer'] = port.manufacturer
+    serialports[port.name]['hwid'] = port.hwid
+
+## Add adapters to tree
+
+adattree = configui.adapterselect
 
 ## If add fixture button pressed, run addfixtureprofile()
 configui.addfixtureprofile.clicked.connect(runaddfixtureprofile)
