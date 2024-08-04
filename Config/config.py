540dfb56a5676c3b328931bd52eb432a721faadf
Stavro Purdie <stavropurdie9@gmail.com>
2024-08-05 10:36:52 +1200

    changed what system the GUI uses to load (load in from py file)

--- Config/config.py
+++ Config/config.py
@@ -1,59 +1,16 @@
-## This program runs the config GUI
-from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import QFile, QIODevice
-import serial.tools.list_ports
 import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QFile
+from maingui import *
 
+def addfixtureprofile():
+    print("test")
 
-def runaddfixtureprofile():
-    addfixtureprofile.show()
-
-## Init GUI files....
-app = QApplication(sys.argv)
-loader = QUiLoader()
-
-## Main config UI....
-mainconfiguifile = QFile("Config.ui")
-configui = loader.load(mainconfiguifile)
-mainconfiguifile.close()
-if not configui:
-    print(configui.errorString())
-    sys.exit(-1)
-
-## Add Fixture Profile UI....
-addfixtureprofilefile = QFile("addfixtureprofile.ui")
-addfixtureprofile = loader.load(addfixtureprofilefile)
-addfixtureprofilefile.close()
-if not addfixtureprofile:
-    print(addfixtureprofile.errorString())
-    sys.exit(-1)
-
-## Fixture Patch UI....
-fixturepatchfile = QFile("fixturepatch.ui")
-fixturepatch = loader.load(fixturepatchfile)
-fixturepatchfile.close()
-if not fixturepatchfile:
-    print(fixturepatchfile.errorString())
-    sys.exit(-1)
-
-## Fire up the GUI and show it
-configui.show()
-
-## Adapter Selection Init (Get all adapters)
-
-serialports = {}
-for port in serial.tools.list_ports.comports():
-    serialports[port.name] = {}
-    serialports[port.name]['description'] = port.description
-    serialports[port.name]['manufacturer'] = port.manufacturer
-    serialports[port.name]['hwid'] = port.hwid
-
-## Add adapters to tree
-
-adattree = configui.adapterselect
-
-## If add fixture button pressed, run addfixtureprofile()
-configui.addfixtureprofile.clicked.connect(runaddfixtureprofile)
-
-sys.exit(app.exec())
+class MainWindow(QMainWindow):
+    app = QtWidgets.QApplication(sys.argv)
+    Wizard = QtWidgets.QWizard()
+    ui = Ui_Wizard()
+    ui.setupUi(Wizard)
+    Wizard.show()
+    Wizard.addfixtureprofile.clicked.connect(addfixtureprofile)
+    sys.exit(app.exec())
