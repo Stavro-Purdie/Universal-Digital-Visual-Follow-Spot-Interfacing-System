1f2769d5e10c46e73e3696b130ce79dc20f592a0
Stavro Purdie <stavropurdie9@gmail.com>
2024-08-05 12:28:24 +1200

    Worked more on backend. CHANGED STRUCTURE AGAIN

--- Config/config.py
+++ Config/config.py
@@ -1,16 +1,27 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QFile
-from maingui import *
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
 
-def addfixtureprofile():
-    print("test")
+## Init section
+app = QApplication(sys.argv)
+loader = QUiLoader()
 
-class MainWindow(QMainWindow):
-    app = QtWidgets.QApplication(sys.argv)
-    Wizard = QtWidgets.QWizard()
-    ui = Ui_Wizard()
-    ui.setupUi(Wizard)
-    Wizard.show()
-    Wizard.addfixtureprofile.clicked.connect(addfixtureprofile)
-    sys.exit(app.exec())
+## Main config UI
+configuifile = QFile("Config.ui")
+if not configuifile.open(QIODevice.ReadOnly):
+    print(f"Cannot open Config.ui: {configuifile.errorString()}")
+    sys.exit(-1)
+configui = loader.load(configuifile)
+configuifile.close()
+if not configui:
+    print(loader.errorString())
+    sys.exit(-1)
+
+## Add Fixture Profile UI
+afpuifile = QFile("addfixtureprofile.ui")
+
+## Show configui
+configui.show()
+
+sys.exit(app.exec())
